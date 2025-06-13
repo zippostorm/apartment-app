66d78dda5ea4b98ee3ec52589f544aea0d20d8c9
zippostorm <toghkklli@gmail.com>
2025-06-13 10:42:59 +0300

    Navbar complete + react router dom setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <div className="min-h-screen bg-base-200 transition-colors duration-300 overflow-hidden">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link, useResolvedPath } from "react-router-dom";
+import { House, PlusCircleIcon } from "lucide-react";
+
+const Navbar = () => {
+  const { pathname } = useResolvedPath();
+  const isHomePage = pathname === "/";
+  return (
+    <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto">
+        <div className="navbar px-4 min-h-[4rem] justify-between">
+          {/* LOGO */}
+          <div className="flex-1 lg:flex-none">
+            <Link to="/" className="hover:opacity-80 transition-opacity">
+              <div className="flex items-center gap-2">
+                <House className="size-8 text-primary" />
+                <span
+                  className="font-semibold font-mono tracking-widest text-2xl 
+                bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
+                >
+                  FLATBOARD
+                </span>
+              </div>
+            </Link>
+          </div>
+
+          {isHomePage && (
+            <div className="flex items-center gap-4">
+              <button
+                className="btn btn-primary"
+                onClick={() =>
+                  document.getElementById("add_apartment_modal").showModal()
+                }
+              >
+                <PlusCircleIcon className="size-5" />
+                <span className="hidden sm:inline ml-2">Add Apartment</span>
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
